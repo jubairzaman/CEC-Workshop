7d10c6c09658839c308cc6142b54f6627d0c674c
NSUIT-LAB <jubair.zaman01@gmail.com>
2019-02-08 16:39:41 +0600

    BioData

---
+++ src/day2/BioData.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class BioData {
+
+    public static void main (String[]args) {
+
+        System.out.println("Name : Jubair ");
+        System.out.println("Id : 1711981642");
+        System.out.println("Dept : ECE");
+
+    }
+
+
+}
+
+
+
